5b17904f735b7031ce840bd796a83c39f1bee604
jdmcrew <german1992fm@hotmail.com>
2017-02-14 20:03:31 +0100

    Update CameraSourcePreview.java

--- app/src/main/java/com/camera/reader/CameraSourcePreview.java
+++ app/src/main/java/com/camera/reader/CameraSourcePreview.java
@@ -14,7 +14,7 @@ import com.google.android.gms.vision.CameraSource;
 import java.io.IOException;
 
 /**
- * Created by German on 14/02/2017.
+ * Created by German on 14/02/2017
  */
 
 public class CameraSourcePreview extends ViewGroup {
